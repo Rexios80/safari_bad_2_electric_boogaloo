532ed7c28540d8d2c3bd707b1ce7c5dc25f13735
Rexios <rexios@rexios.dev>
2022-03-16 21:18:23 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SafariBad());
+}
+
+class SafariBad extends StatelessWidget {
+  const SafariBad({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Safari Bad')),
+        body:  Center(
+          child: Column(children: [
+            CircularProgressIndicator(),
+          ]),
+        ),
+      ),
+    );
+  }
+}
